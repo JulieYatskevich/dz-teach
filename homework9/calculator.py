24fed25f3e70d8e2b21d8f133440e5cdfbed1e7f
admin <admin@MacBook-admin.local>
2022-04-19 15:18:37 +0300

    new calc

--- homework9/calculator.py
+++ homework9/calculator.py
@@ -1,46 +1,42 @@
 class Calculator:
 
-    print("Выбери действия.\n1.Сложение\n2.Вычитание\n3.Умножение\n4.Деление")
+    @staticmethod
+    def choice_action():
+        print("Выбери действия.\n1.Сложение\n2.Вычитание\n3.Умножение\n4.Деление")
 
 
-    def add(x, y: float):
-        return x + y
+    @staticmethod
+    def add_choice():
+        while True:
+            try:
+                choice = input("Enter choice: ")
 
-    def sub(x, y: float):
-        return x - y
+                if choice in ('1', '2', '3', '4'):
+                    number1 = float(input("Enter first number: "))
+                    number2 = float(input("Enter second number: "))
 
-    def mult(x, y: float):
-        return x * y
+                    if choice == '1':
+                        print(number1, "+", number2, "=", float(number1+number2))
 
-    def divide(x, y: float):
-        return x / y
+                    elif choice == '2':
+                        print(number1, "-", number2, "=", float(number1-number2))
+
+                    elif choice == '3':
+                        print(number1, "*", number2, "=", float(number1*number2))
+
+                    elif choice == '4':
+                        print(number1, "/", number2, "=", float(number1/number2))
+                else:
+                    print("Неверный выбор")
+            except ZeroDivisionError:
+                print("На 0 делить нельзя")
+                continue
+            except ValueError:
+                print("Введи число")    
+                continue
+
+    
 
 
-
-    while True:
-        try:
-            choice = input("Enter choice: ")
-
-            if choice in ('1', '2', '3', '4'):
-                number1 = float(input("Enter first number: "))
-                number2 = float(input("Enter second number: "))
-
-                if choice == '1':
-                    print(number1, "+", number2, "=", add(number1, number2))
-
-                elif choice == '2':
-                    print(number1, "-", number2, "=", sub(number1, number2))
-
-                elif choice == '3':
-                    print(number1, "*", number2, "=", mult(number1, number2))
-
-                elif choice == '4':
-                    print(number1, "/", number2, "=", divide(number1, number2))
-            else:
-                print("Неверный выбор")
-        except ZeroDivisionError:
-            print("На 0 делить нельзя")
-            continue
-        except ValueError:
-            print("Введи число")    
-            continue
+Calculator.choice_action()
+Calculator.add_choice()
