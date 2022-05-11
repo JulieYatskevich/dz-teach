6c16d6e1fa4f9502dceed4c9560bc523006fc4ad
admin <admin@MacBook-admin.local>
2022-05-11 23:24:48 +0300

    flasknew

--- event/event.py
+++ event/event.py
@@ -1,8 +1,6 @@
 import os
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-
-from sqlalchemy.sql import func
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -15,66 +13,35 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class Obrabotka(db.Model):
+master_part = db.Table('master_part',
+    db.Column('part_id', db.Integer, db.ForeignKey('participants.id')),
+    db.Column('master_id', db.Integer, db.ForeignKey('masterklass.id'))
+)
+
+class Masterklass(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<Masterklass {self.name}>'
+    
+
+class Participants(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
     phone = db.Column(db.String, nullable=False)
+    following = db.relationship('Masterklass', secondary=master_part, backref='followers')
+
 
     def __repr__(self):
-        return f'<Obrabotka {self.first_name}>'
-
-
-class Osnovi(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    first_name1 = db.Column(db.String, nullable=False)
-    last_name1 = db.Column(db.String, nullable=False)
-    phone1 = db.Column(db.String, nullable=False)
-
-    def __repr__(self):
-        return f'<Osnovi {self.first_name1}>'
+        return f'<Participants {self.first_name}>'
 
 
 @app.route('/')
 def index():
         return render_template ("index.html")
-
-
-@app.route('/obrabotka-foto',  methods=['GET', 'POST'])
-def obr_foto():
-    if request.method == "POST":
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        phone = request.form['phone']
-
-        obrabotka = Obrabotka(first_name=first_name, last_name=last_name, phone=phone)
-
-        try:
-            db.session.add(obrabotka)
-            db.session.commit()
-            return redirect ('/')
-        except:
-            return "При добавлении произошла ошибка"
-    return render_template ("obrabotka.html")
-
-
-@app.route('/osnovi-foto',  methods=['GET', 'POST'])
-def osnovi():
-    if request.method == "POST":
-        first_name1 = request.form['first_name1']
-        last_name1 = request.form['last_name1']
-        phone1 = request.form['phone1']
-        
-        osnovi = Osnovi(first_name1=first_name1, last_name1=last_name1, phone1=phone1)
-        
-        try:
-            db.session.add(osnovi)
-            db.session.commit()
-            return redirect ('/')
-        except:
-            return "При добавлении произошла ошибка"
-
-    return render_template ("osnovi.html")
 
 
 if __name__ == "__main__":
